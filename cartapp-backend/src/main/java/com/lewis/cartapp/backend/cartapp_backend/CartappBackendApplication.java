3a7c7eb865b6345bb2bb07f6da4c259012e3f21b
lewislpz <lewislpz@protonmail.com>
2026-03-11 06:39:07 +0100

    initial commit

---
+++ cartapp-backend/src/main/java/com/lewis/cartapp/backend/cartapp_backend/CartappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.lewis.cartapp.backend.cartapp_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartappBackendApplication.class, args);
+	}
+
+}
